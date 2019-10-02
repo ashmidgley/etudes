492e41ac227b0ba1ab21eefd4c8a0e8f38cd4676
Ash Midgley <ayelgdom@tutanota.com>
2019-10-02 14:27:18 +0100

    Adding summary pdf's and missing etudes.

---
+++ 02-rollin-and-summit/Battlers.java
@@ -0,0 +1,12 @@
+package rollin;
+
+public class Battlers extends Rollin{
+
+    public static void main(String[] args){
+        Rollin roll = new Rollin();
+    }
+
+    public int handleRoll(int roll){
+        
+    }
+}
